ae1ed614a38b86cabe5119f5ff4cde3dc30ff715
what-the-sherlock <aashu0333@gmail.com>
2026-07-20 20:41:28 +0530

    removed unwanted/not used assets

--- src/RedirectToApiDocs.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react';
-
-export default function RedirectToApiDocs() {
-  useEffect(() => {
-    window.location.href = 'https://app.dros.ai/api-docs';
-  }, []);
-
-  return null;
-}
--- src/RedirectToReleaseNotes.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react';
-
-export default function RedirectToReleaseNotes() {
-  useEffect(() => {
-    window.location.href = 'https://app.dros.ai/release-notes';
-  }, []);
-
-  return null;
-}
